ed47fdab083c4f1f58368f11853b421b37eb3744
jihye2424 <wlgp2062@naver.com>
2025-07-08 17:46:03 +0900

    instareview

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.scss";
 import Sale from "./components/Sale";
+import InstaReview from "./components/InstaReview";
 
 const App = () => {
   return (
     <BrowserRouter>
     <div id="App">
       <Routes>
-        <Route
+         <Route
           path="/"
           element={
             <Sale />
           }
-        />
+        /> 
+        
       </Routes>
+      <InstaReview/> 
       </div>
     </BrowserRouter>
   );
