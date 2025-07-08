fba1951cbd822e5af12e2ff5021f3110de8b97f8
jominji96 <wkqzodyd@naver.com>
2025-07-08 16:48:24 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.scss";
-import Sale from "./components/Sale";
 
 const App = () => {
   return (
-    <BrowserRouter>
-    <div id="App">
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Sale />
-          }
-        />
-      </Routes>
-      </div>
-    </BrowserRouter>
+    <div>
+
+    </div>
   );
 };
 
