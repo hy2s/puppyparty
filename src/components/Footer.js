5d472d6edb29d91bda06d833e83e2884627c65c7
hy2s <rlagptmd1021@naver.com>
2025-07-14 14:56:35 +0900

    FINAL

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,7 +17,7 @@ const Footer = () => {
                     </ul>
                 </div>
                 <div className="two">
-                    <img src="/images/logo.png" />
+                    <img src={`${process.env.PUBLIC_URL}/images/logo.png`} />
                     <p>© 2025 (Puppy Party). All rights reserved.</p>
                 </div>
             </div>
